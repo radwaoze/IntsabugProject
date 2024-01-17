cd4341273ac5ee2c8885e61bb7f1f32f152b4723
radwa <radwaelsayedfcih@gmail.com>
2024-01-18 00:43:46 +0200

    tagsPart

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,8 @@ import tests.TestBase;
 
 @CucumberOptions(features = "src/test/java/features",
         glue = {"steps"},
-        plugin = {"pretty" , "html:target/cucumber-html-report"})
+        tags = "@E2E or @lockedUser or @invalidCredentials or @errorUser or @problemUser"
+)
 public class TestRunner extends TestBase {
 
 
